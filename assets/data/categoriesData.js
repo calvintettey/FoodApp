47a62dccbf2ab0c996f200b52e9c5afd16219af8
Calvin <63000172+calvintettey@users.noreply.github.com>
2021-05-18 23:34:51 +0000

    Create categoriesData.js

---
+++ assets/data/categoriesData.js
@@ -0,0 +1,38 @@
+const categoriesData = [
+  {
+    id: "1",
+    image: require("../images/pizza.png"),
+    title: "Pizza",
+    selected: true,
+  },
+
+  {
+    id: "2",
+    image: require("../images/shrimp.png"),
+    title: "Seafood",
+    selected: false,
+  },
+
+  {
+    id: "3",
+    image: require("../images/cola.png"),
+    title: "Soft Drinks",
+    selected: false,
+  },
+
+  {
+    id: "4",
+    image: require("../images/salad.png"),
+    title: "Salads",
+    selected: false,
+  },
+
+  {
+    id: "5",
+    image: require("../images/burrito.png"),
+    title: "Burrito",
+    selected: false,
+  },
+];
+
+export default categoriesData;
